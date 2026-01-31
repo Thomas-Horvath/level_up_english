f3bc3997d96b620a2c286189a851a2f17839fd14
Thomas-Horvath <thomas.horvath0416@gmail.com>
2026-01-31 19:58:07 +0100

    hamburger menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,13 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { GiHamburgerMenu } from "react-icons/gi";
+import { HiMenu, HiX } from "react-icons/hi";
+import { useState } from 'react';
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
+
+
     return (
         <header className="fixed top-0 z-50 w-full border-b border-neutral-200 bg-white/90 backdrop-blur">
             <div className="items-center flex justify-between mx-auto h-20 max-w-[1400px] px-6">
@@ -47,7 +51,40 @@ const Navbar = () => {
                         Ingyenes 15 perces konzultáció
                     </Link>
                 </div>
-                <GiHamburgerMenu className="md:hidden" />
+
+
+
+
+                {/* Mobile hamburger */}
+                <button onClick={() => setOpen(!open)} className="md:hidden text-3xl text-white cursor:pointer">
+                    {open ? <HiX className='cursor-pointer text-slate-900' /> : <HiMenu className='cursor-pointer text-slate-900' />}
+                </button>
+
+                {
+                    open && (
+                        <div className="absolute left-0 md:hidden top-20
+                                        w-full
+                                        bg-white border-b border-neutral-200 border-t">
+                            <nav className="flex flex-col items-center gap-4 py-6 font-medium text-md text-slate-600">
+                                <Link onClick={() => setOpen(!open)} href="/#how-it-works" className="hover:text-slate-900">
+                                    Hogyan működik
+                                </Link>
+                                <Link onClick={() => setOpen(!open)} href="/#packages" className="hover:text-slate-900">
+                                    Csomagok
+                                </Link>
+                                <Link onClick={() => setOpen(!open)} href="/#testimonials" className="hover:text-slate-900">
+                                    Vélemények
+                                </Link>
+                                <Link onClick={() => setOpen(!open)} href="/#faq" className="hover:text-slate-900">
+                                    GYIK
+                                </Link>
+                            </nav>
+                        </div>
+                    )
+                }
+
+
+
             </div>
         </header>
     );
